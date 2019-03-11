4896c3a45b156ad2e6ea19386e019197acfa27b1
MsJacksonIYN <sjackson2010@gmail.com>
2019-03-11 17:23:11 -0400

    updated functions

--- functions.py
+++ functions.py
@@ -1,17 +1,25 @@
 # Make a function to determine if a number is odd or even
 
-def odd_even():
-    pass
+def odd_even(num):
+    if num % 2 == 0:
+        return "even"
+    else:
+        return "odd"
 
 # Make a function that takes in a list of numbers and returns the numbers that are even
 
 def even_list(numbers):
-    pass
+    even_list = []
+    for n in numbers:
+        if odd_even(n) == "even":
+            even_list.append(n)
+    return even_list
 
 # Given a list return the unique names in the list
 
 def unique_names(list_of_names):
-    pass
+    namelist = set(list_of_names)
+    return namelist
 
 # Make a function that determines if a word is a palindrome
 
@@ -19,7 +27,10 @@ def palindrome_detector(string):
     """
     Input: string
     returns : True/False"""
-    pass
+    letter_list = list(string)
+    letter_list.reverse()
+    new_string = "".join(letter_list)
+    return new_string == string
 
 print(odd_even(4))
 print(odd_even(139))
